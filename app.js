4f8232eceb9aa16a17dae329de1d75dad3fad8b0
Jaan19867 <123554945+Jaan19867@users.noreply.github.com>
2023-06-08 16:37:15 +0530

    this is my second commit

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const http=require('http');
+const http = require("http");
 
-const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method, req.headers);
-   
+const server = http.createServer((req, res) => {
+  // console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
 
+    res.write(`
+    <head>
+      <title>Enter First Page</title>
+    </head>
+`);
 
-})
+    res.write("<body><form>input type='text'><button type='<body>");
 
+    res.end();
+  }
+  res.setHeader("Conten-Type", "text/html");
+
+  res.write("<html>");
+
+  res.write(`
+    <head>
+      <title>My First Page</title>
+    </head>
+`);
+
+  res.write("<body><h1> Hello from my Node.js Server!</h1><body>");
+
+  res.end();
+});
 server.listen(3000);
-
